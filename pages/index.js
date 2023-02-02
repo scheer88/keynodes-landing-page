4fca637cac61d4ef5088d52f0ca9abfac56e946a
Jonas Scheer <jonas@jscheer.de>
2023-02-02 19:57:05 +0100

    change page title and dev port

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import SeoHead from "../components/SeoHead";
 export default function Home() {
   return (
     <>
-      <SeoHead title='LaslesVPN Landing Page' />
+      <SeoHead title='Keynodes.xyz - Non-Custodial Staking' />
       <Layout>
         <Hero />
         <Feature />
